ae47541e29b1e40aa6139e80467d04e0d7bdbcea
cwang64 <cwang64@ncsu.edu>
2020-10-05 20:04:45 -0400

    rearrange and export tf-idf featured data

--- code/data-processing.py
+++ code/data-processing.py
@@ -1,57 +1,109 @@
-# import os
-# from chardet.universaldetector import UniversalDetector
-#
-#
-# # def get_encode_info(file):
-# #     with open(file, 'rb') as f:
-# #         detector = UniversalDetector()
-# #         for line in f.readlines():
-# #             detector.feed(line)
-# #             if detector.done:
-# #                 break
-# #         detector.close()
-# #         return detector.result['encoding']
-# #
-# #
-# # def convert_encode2utf8(file):
-# #     f = open(file, 'rb')
-# #     file_content = f.read()
-# #     encode_info = get_encode_info(file)
-# #     if encode_info == 'utf-8':
-# #         return
-# #     file_decode = file_content.decode(encode_info, 'ignore')
-# #     file_encode = file_decode.encode('utf-8')
-# #     f.close()
-# #     f = open(file, 'wb')
-# #     f.write(file_encode)
-# #     f.close()
-#
-# # 把data讀進來
-# def read_file():
-#     names = os.listdir('/Users/bibi/Documents/CSC522/project/maildir')
-#     X = []
-#     Y = []
-#
-#     for idx, name in enumerate(names):
-#         allList = os.walk('/Users/bibi/Documents/CSC522/project/maildir/' + name)
-#         for root, dirs, files in allList:
-#             for file in files:
-#                 file_name = root + '/' + file
-#
-#                 # transform encoding to utf-8
-#                 # convert_encode2utf8(file_name)
-#
-#                 f = open(file_name, 'rb')
-#                 X.append(f.read())
-#                 f.close()
-#                 Y.append(idx)
-#     return X, Y
-#
-#
-# def split_space(X):
-#     for idx, x in enumerate(X):
-#         X[idx] = x.split(" ")
-#     return X
-#
-# def run_tfidf():
-#
+import os
+from chardet.universaldetector import UniversalDetector
+import pandas as pd
+import numpy as np
+import re
+from nltk.corpus import stopwords
+from tfidf import *
+
+
+def get_encode_info(file):
+    with open(file, 'rb') as f:
+        detector = UniversalDetector()
+        for line in f.readlines():
+            detector.feed(line)
+            if detector.done:
+                break
+        detector.close()
+        return detector.result['encoding']
+
+
+def convert_encode2utf8(file):
+    f = open(file, 'rb')
+    file_content = f.read()
+    encode_info = get_encode_info(file)
+    if encode_info == 'utf-8':
+        return
+    file_decode = file_content.decode(encode_info, 'ignore')
+    file_encode = file_decode.encode('utf-8')
+    f.close()
+    f = open(file, 'wb')
+    f.write(file_encode)
+    f.close()
+
+
+def read_name(idx, name, x, y, path):
+    allList = os.walk(path + '\\' + name)
+    for root, dirs, files in allList:
+        for file in files:
+            file_name = root + '/' + file
+
+            # transform encoding to utf-8
+            convert_encode2utf8(file_name)
+
+            f = open(file_name, 'rb')
+            x.append(f.read())
+            f.close()
+            y.append(idx)
+
+
+def read_dataSet():
+    # data set path
+    path = r'C:\Users\ericb\Desktop\maildir'
+    names = os.listdir(path)
+    x = []
+    y = []
+
+    for idx, name in enumerate(names):
+        read_name(idx, name, x, y, path)
+
+    return x, y, names
+
+
+def remove_sp_symbol(text):
+    comp = re.compile('[^A-Z^a-z^0-9^ ]')
+    return comp.sub(' ', text)
+
+
+# remove special symbol, low idf words and stop words
+def extract_feature(x):
+    x_featured = []
+    stop_words = set(stopwords.words('english'))
+    idf_low = get_idf_low()
+    for text in x:
+        textList = remove_sp_symbol(text).lower().split(" ")
+        for idx, t in enumerate(textList):
+            if t in idf_low or t in stop_words or t == 'x':
+                textList[idx] = ''
+        x_featured.append(' '.join(textList))
+    return x_featured
+
+
+def export_featured_data():
+    # xTest = []
+    # y = []
+    # al = os.walk(r'C:\Users\ericb\Desktop\maildir/presto-k/junk_e_mail/')
+    # for root, dirs, files in al:
+    #     for file in files:
+    #         file_name = root + '/' + file
+    #         f = open(file_name, 'r')
+    #         xTest.append(f.read())
+    #         f.close()
+    #         y.append('presto-k')
+
+    x, y, y_class = read_dataSet()
+    x = extract_feature(x)
+    data = list(zip(x, y))
+    df = pd.DataFrame(data=np.asarray(data))
+    df.to_csv('../data.csv')
+    return y_class
+
+
+def main():
+    # y = export_featured_data()
+    # train_w2v()
+    data_dir = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), "data/Q3.csv")
+
+
+if __name__ == "__main__":
+    main()
--- code/tfidf.py
+++ code/tfidf.py
@@ -1,120 +1,29 @@
-import os
-from chardet.universaldetector import UniversalDetector
-from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
-import re
-from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def get_encode_info(file):
-    with open(file, 'rb') as f:
-        detector = UniversalDetector()
-        for line in f.readlines():
-            detector.feed(line)
-            if detector.done:
-                break
-        detector.close()
-        return detector.result['encoding']
-
-
-def convert_encode2utf8(file):
-    f = open(file, 'rb')
-    file_content = f.read()
-    encode_info = get_encode_info(file)
-    if encode_info == 'utf-8':
-        return
-    file_decode = file_content.decode(encode_info, 'ignore')
-    file_encode = file_decode.encode('utf-8')
-    f.close()
-    f = open(file, 'wb')
-    f.write(file_encode)
-    f.close()
-
-
-def read_dataSet():
-    names = os.listdir(r'C:\Users\ericb\Desktop\maildir')
-    X = []
-    Y = []
-
-    for idx, name in enumerate(names):
-        read_name(idx, name, X, Y)
-
-    return X, Y
-
-
-def read_name(idx, name, X, Y):
-    allList = os.walk(r'C:\Users\ericb\Desktop\maildir\\' + name)
-    for root, dirs, files in allList:
-        for file in files:
-            file_name = root + '/' + file
-
-            # transform encoding to utf-8
-            convert_encode2utf8(file_name)
-
-            f = open(file_name, 'rb')
-            X.append(f.read())
-            f.close()
-            Y.append(idx)
-
-
-# f = open('/Users/bibi/Documents/CSC522/project/maildir/presto-k/junk_e_mail/1.', 'rb')
-# s = f.read()
-# f.close()
-
-# xTest = []
-#
-# al = os.walk(r'C:\Users\ericb\Desktop\maildir/presto-k/junk_e_mail/')
-# for root, dirs, files in al:
-#     for file in files:
-#         file_name = root + '/' + file
-#         f = open(file_name, 'r')
-#         xTest.append(f.read())
-#         f.close()
-
-
-def get_idf(X):
+def get_idf(x):
     tfidf_vectorizer = TfidfVectorizer(analyzer='word', stop_words='english')
-    tfidf_vectorizer.fit_transform(X)
+    tfidf_vectorizer.fit_transform(x)
     tfidf_tokens = tfidf_vectorizer.get_feature_names()
 
     idf = tfidf_vectorizer.idf_
     idf_list = zip(tfidf_tokens, idf)
 
-    idf_list_sorted = sorted(idf_list, key=lambda x: x[1])
+    idf_list_sorted = sorted(idf_list, key=lambda a: a[1])
     return idf_list_sorted
 
 
 def get_idf_low():
+    # idf = get_idf(x)
+    # idf_df = pd.DataFrame(data=np.asarray(idf))
+    # idf_df.to_csv('../idf.csv')
+    # idf_df.head(36).loc[:, '0': '1'].to_csv('../idf_low.csv')
     idf_low = pd.read_csv('../idf_low.csv')
     idf_low_set = set(idf_low['0'])
     return idf_low_set
 
-
-def remove_sp_symbol(text):
-    comp = re.compile('[^A-Z^a-z^0-9^ ]')
-    return comp.sub(' ', text)
-
-
-# remove special symbol, low idf words and stop words
-def extract_feature(X):
-    x_feature = []
-    stop_words = set(stopwords.words('english'))
-    idf_low = get_idf_low()
-    for text in X:
-        textList = remove_sp_symbol(text).lower().split(" ")
-        for idx, t in enumerate(textList):
-            if t in idf_low or t in stop_words or t == 'x':
-                textList[idx] = ''
-        x_feature.append(' '.join(textList))
-    return x_feature
-
-
-qewx = extract_feature(xTest)
-print(qewx[0])
-
-# x, y = read_dataSet()
-# idf = get_idf(x)
-# print(np.asarray(idf[:20]))
+# first 20 idf
 # [['bcc' '1.0']
 #  ['cc' '1.0']
 #  ['charset' '1.0']
@@ -136,7 +45,7 @@ print(qewx[0])
 #  ['transfer' '1.0']
 #  ['type' '1.0']]
 
-# print(np.asarray(idf[len(idf) - 21:]))
+# last 20 idf
 # [['zzp' '13.463428233647901']
 #  ['zzsygxgx' '13.463428233647901']
 #  ['zzsyjrxe' '13.463428233647901']
@@ -159,6 +68,3 @@ print(qewx[0])
 #  ['zzztalk' '13.463428233647901']
 #  ['zzzugorq00c' '13.463428233647901']
 #  ['åkesson' '13.463428233647901']]
-
-# idf_df = pd.DataFrame(data=np.asarray(idf))
-# idf_df.to_csv('../idf.csv')
