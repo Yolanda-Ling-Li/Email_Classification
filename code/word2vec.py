67e98f7ceb8a9e5a7a3ccbe63de3e2db52e21f6e
LingLi <lli46@ncsu.edu>
2020-10-29 17:07:06 -0400

    delete unnecessary code

--- code/word2vec.py
+++ code/word2vec.py
@@ -12,10 +12,6 @@ class Config():
     max_len = 256  # 句长
     vocabulary_dim = 1  # 词向量维数
     min_count = 5  # 过滤频数小于5的词语
-<<<<<<< HEAD
-=======
-    max_vocab_size = 100000  # 如果所有独立单词个数超过这个，则就消除掉其中最不频繁的一个。
->>>>>>> b228f054c9844859970af00e676fcb320b1f6142
     window_size = 8  # 窗口大小
     n_iterations = 5  # 迭代次数，默认为5 #定义词向量模型
     cpu_count = multiprocessing.cpu_count()
